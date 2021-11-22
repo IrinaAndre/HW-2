31b48a2a56fb357984beab7fc266f933e847d211
IrinaAndre <vit_andre@mail.ru>
2021-11-22 15:52:05 +0300

    HW-2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const firstNumber = +prompt('Введите первое число');
+const secondNumber = +prompt('Введите второе число');
+
+console.log(`Результат: ${firstNumber} + ${secondNumber} = (firstNumber + secondNumber))`);
+console.log(`Результат: ${firstNumber} - ${secondNumber} = (firstNumber - secondNumber))`);
+console.log(`Результат: ${firstNumber} * ${secondNumber} = (firstNumber * secondNumber))`);
+console.log(`Результат: ${firstNumber} / ${secondNumber} = (firstNumber / secondNumber))`);
